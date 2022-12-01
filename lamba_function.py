0b6c7f79fcd04d915f5294da6b4ed6d6d684bf69
naren <naren@gmail.com>
2022-12-01 16:57:04 +0530

    new branch update-nonprod

--- lamba_function.py
+++ lamba_function.py
@@ -1,11 +1,13 @@
-print("hi,narendra")
 
-print("hi,rakshu")
+print("welcome to CICD DEMO")
 
 print("Hi,this is CICD DEMO1")
 print("Hi,this is CICD DEMO2")
 print("Hi,this is CICD DEMO3")
 print("Hi,this is CICD DEMO4")
 print("Hi,this is CICD DEMO5")
+print("Hi,this is CICD DEMO6")
+print("Hi,this is CICD DEMO7")
+print("Hi,this is CICD DEMO8")
 
 print("git change sucessfully cloned to ec2 instance")
