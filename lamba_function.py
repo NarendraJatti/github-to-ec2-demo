8e9148458c165c46cefaae9922b51fff78747d85
naren <naren@gmail.com>
2022-12-01 13:09:42 +0530

    naren update2 on 01dec1

--- lamba_function.py
+++ lamba_function.py
@@ -1,9 +1,10 @@
-print("hi,narendra")
 
-print("hi,rakshu")
 
 print("Hi,this is CICD DEMO1")
 print("Hi,this is CICD DEMO2")
 print("Hi,this is CICD DEMO3")
+print("Hi,this is CICD DEMO4")
+print("Hi,this is CICD DEMO5")
+print("Hi,this is CICD DEMO6")
 
 print("git change sucessfully cloned to ec2 instance")
