493eab56c93ddcd9d3ca97ca689aa0d8038fc467
naren <naren@gmail.com>
2022-12-02 12:09:40 +0530

    main branch updated by naren on 02dec at 12am

--- lamba_function.py
+++ lamba_function.py
@@ -1,7 +1,7 @@
-print("welcome to CICD Demo")
+print("Welcome to CICD Demo")
 
 print("Hi,this is CICD DEMO1")
 print("Hi,this is CICD DEMO2")
-print("Hi,this is CICD DEMO3")
+
 
 print("git change sucessfully cloned to ec2 instance")
